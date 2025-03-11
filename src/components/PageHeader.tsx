3c02aa5ca6a2ed4b827729119d2083e6b52b693c
marcpanu <marc.panu@gmail.com>
2025-03-11 16:38:30 -0400

    updated font 4

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -5,7 +5,7 @@ interface PageHeaderProps {
 export const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
   return (
     <div className="px-4 py-6 bg-white border-b border-zinc-200">
-      <h1 className="text-2xl font-bold text-zinc-900">{title}</h1>
+      <h1 className="text-2xl font-extrabold text-zinc-900">{title}</h1>
     </div>
   );
 }; 
