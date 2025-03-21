6ecc7dfc814a99cf5366a0484cc9d270a68df841
marcpanu <marc.panu@gmail.com>
2025-03-20 22:40:22 -0400

    updated meal selector UI component

--- src/components/mealPlan/ScheduleMealModal.tsx
+++ src/components/mealPlan/ScheduleMealModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Dialog } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { Recipe } from '../../types/recipe';
